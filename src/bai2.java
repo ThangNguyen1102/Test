9dfdaacac7c149e22b4ef6add0eee40a6d027802
ThangNguyen1102 <73435399+ThangNguyen1102@users.noreply.github.com>
2022-06-22 12:12:40 +0700

    Update bai2.java

--- src/bai2.java
+++ src/bai2.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class bai2 {
     public static void main(String[] args)
     {
-        float sodien, total;
+        float soDien, total;
         Scanner scanner = new Scanner(System.in);
 
-        int x1 = 1000, x2 = 1250, x3 = 1800, x4 = 2500;
+        int donGia1 = 1000, donGia2 = 1250, donGia3 = 1800, donGia4 = 2500;
         System.out.println("hello");
         do {
             System.out.print("Nhap so dien trong 1 thang: ");
@@ -15,22 +15,22 @@ public class bai2 {
             {
                 System.out.println("So dien khong hop le, vui long nhap lai!");
             }
-        }while(sodien < 0);
-        if(sodien <= 25)
+        }while(soDien < 0);
+        if(soDien <= 25)
         {
-            total = sodien * x1;
+            total = soDien * donGia1;
             System.out.println("Tien dien phai tra la: " + total);
         }
-        else if (sodien <= 75) {
-            total = 25 * x1 + (sodien - 25) * x2;
+        else if (soDien <= 75) {
+            total = 25 * donGia1 + (soDien - 25) * donGia2;
             System.out.println("Tien dien phai tra la: " + total);
         }
-        else if (sodien <= 150){
-            total = 25 * x1 + 50 * x2 + (sodien - 75) * x3;
+        else if (soDien <= 150){
+            total = 25 * donGia1 + 50 * donGia2 + (soDien - 75) * donGia3;
             System.out.println("Tien dien phai tra la: " + total);
         }
-        else if (sodien > 150){
-            total = 25 * x1 + 50 * x2 + 75 * x3 + (sodien - 150) * x4;
+        else if (soDien > 150){
+            total = 25 * donGia1 + 50 * donGia2 + 75 * donGia3 + (soDien - 150) * donGia4;
             System.out.println("Tien dien phai tra la: " + total);
         }
     }
