be8ef577b81b95abe9d7b7c359fd134277d5efd7
ThangNguyen1102 <73435399+ThangNguyen1102@users.noreply.github.com>
2022-06-22 12:10:20 +0700

    Update bai1.java

--- src/bai1.java
+++ src/bai1.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class bai1 {
-    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập hệ số bậc 2, a = ");
-        float a = bai1.scanner.nextFloat();
+        float a = scanner.nextFloat();
         System.out.print("Nhập hệ số bậc 1, b = ");
-        float b = bai1.scanner.nextFloat();
+        float b = scanner.nextFloat();
         System.out.print("Nhập hằng số tự do, c = ");
         float c = scanner.nextFloat();
-
         //tinh delta
         float delta = b*b - 4*a*c;
         System.out.println("Can delta bang: " + Math.sqrt(delta));
