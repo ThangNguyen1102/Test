04770f7c9ab542918b2f252dde7749f51e277afb
ThangNguyen1102 <73435399+ThangNguyen1102@users.noreply.github.com>
2022-06-22 12:09:24 +0700

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -4,13 +4,13 @@ public class Square {
         System.out.println(canh);
     }
 
-    public void chuvi(){
+    public void chuVi(){
         float p;
         p = canh * 4;
         System.out.println("Chu vi hinh vuong la: " + p);
     }
 
-    public void dientich(){
+    public void dienTich(){
         float S;
         S = canh * canh;
         System.out.println("Dien tich hinh vuong la: " + S);
@@ -18,7 +18,7 @@ public class Square {
 
     public void show(){
         showCanh();
-        chuvi();
-        dientich();
+        chuVi();
+        dienTich();
     }
 }
